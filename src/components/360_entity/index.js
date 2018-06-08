69a9de40eea6c86d1cc95fb1b291e81107007208
btp <pbottarella@gmail.com>
2018-06-08 15:37:10 +0200

    current state

--- src/components/360_entity/index.js
+++ src/components/360_entity/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './styles.css';
 import ReactCursorPosition from 'react-cursor-position';
-import Image from '../TestImage';
-//import Image from '../Image';
+import Image from '../Image';
 
 const Entity = () => {
   return (
     <div>
       <ReactCursorPosition className="Wrapper">
         <div className="Header">HEADER</div>
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
+        <Image />
         <Image />
         <Image />
         <Image />
--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './styles.css';
 import image_pos1 from '../../images/pat_pos1.jpg';
 import image_pos2 from '../../images/pat_pos2.jpg';
 import image_pos3 from '../../images/pat_pos3.jpg';
@@ -9,79 +8,113 @@ import image_pos6 from '../../images/pat_pos6.jpg';
 import image_pos7 from '../../images/pat_pos7.jpg';
 import image_pos8 from '../../images/pat_pos8.jpg';
 import default_image from '../../images/pat_default.jpg';
+import ReactDOM from 'react-dom';
+import './styles.css';
 
 
-class Image extends React.Component {
+class TestImage extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      width: window.innerWidth,
-      height: window.innerHeight,
-      imageWidth: 250,
-      imageHeight: 180
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+      imageCoordinates: {
+        width: 0,
+        height: 0,
+        topPosition: 0,
+        leftPosition: 0,
+      }
     };
   }
 
   componentDidMount() {
     // Additionally I could have just used an arrow function for the binding `this` to the component...
     window.addEventListener("resize", this.updateDimensions);
+    window.addEventListener("resize", this.updateImageLocation);
+    this.updateImageLocation();
   }
+
   updateDimensions = () => {
     this.setState({
-      height: window.innerHeight,
-      width: window.innerWidth,
+      screenHeight: window.innerHeight,
+      screenWidth: window.innerWidth,
     });
   }
 
-  renderImage = () => {
-    const x = this.props.position.x;
-    const y = this.props.position.y;
-    const windowWidth = this.state.width;
-    const windowHeight = this.state.height;
-    const imageWidth = this.state.imageWidth;
-    const imageHeight = this.state.imageHeight;
+  updateImageLocation = () => {
+    this.setState({
+      imageCoordinates: {
+        width: parseInt(Number(ReactDOM.findDOMNode(this.refs.image).getBoundingClientRect().width).toFixed(0)),
+        height: parseInt(Number(ReactDOM.findDOMNode(this.refs.image).getBoundingClientRect().height).toFixed(0)),
+        topPosition: Math.round(ReactDOM.findDOMNode(this.refs.image).getBoundingClientRect().top),
+        leftPosition: Math.round(ReactDOM.findDOMNode(this.refs.image).getBoundingClientRect().left),
+      }
+    });
+  }
+
+  getImageSource = () => {
+    const imagePosition = {
+      x: this.state.imageCoordinates.leftPosition,
+      y: this.state.imageCoordinates.topPosition
+    };
+    const imageWidth = this.state.imageCoordinates.width;
+    const imageHeight = this.state.imageCoordinates.height;
+    const cursorPosition = {
+      x: this.props.position.x,
+      y: this.props.position.y
+    };
+    const screenDefinitions = {
+      width: this.state.screenWidth,
+      height: this.state.screenHeight
+    };
 
     //RENDER IMAGE POSITION 8
-    if (x > 0 && x < (windowWidth / 2) - (imageWidth / 2) && y >= 0 && y <= (windowHeight / 2) - (imageHeight / 2)) {
-      return < img className="Image" alt="teamMember" src={image_pos8} />
+    if (cursorPosition.x >= 0 && cursorPosition.x <= imagePosition.x && cursorPosition.y >= 0 && cursorPosition.y <= imagePosition.y) {
+      return image_pos8;
     }
     //RENDER IMAGE POSITION 1
-    else if (x >= (windowWidth / 2) - (imageWidth / 2) && x <= (windowWidth / 2) + (imageWidth / 2) && y >= 0 && y <= (windowHeight / 2) - (imageHeight / 2)) {
-      return < img className="Image" alt="teamMember" src={image_pos1} />
+    else if (cursorPosition.x >= imagePosition.x && cursorPosition.x <= (imagePosition.x + imageWidth) && cursorPosition.y >= 0 && cursorPosition.y <= imagePosition.y) {
+      return image_pos1;
     }
     //RENDER IMAGE POSITION 2
-    else if (x >= (windowWidth / 2) + (imageWidth / 2) && x <= windowWidth && y >= 0 && y <= (windowHeight / 2) - (imageHeight / 2)) {
-      return < img className="Image" alt="teamMember" src={image_pos2} />
+    else if (cursorPosition.x >= (imagePosition.x + imageWidth) && cursorPosition.x <= screenDefinitions.width && cursorPosition.y >= 0 && cursorPosition.y <= imagePosition.y) {
+      return image_pos2;
     }
     //RENDER IMAGE POSITION 3
-    else if (x >= (windowWidth / 2) + (imageWidth / 2) && x <= windowWidth && y >= (windowHeight / 2) - (imageHeight / 2) && y <= (windowHeight / 2) + (imageHeight / 2)) {
-      return <img className="Image" alt="teamMember" src={image_pos3} />
+    else if (cursorPosition.x >= (imagePosition.x + imageWidth) && cursorPosition.x <= screenDefinitions.width && cursorPosition.y >= imagePosition.y && cursorPosition.y <= (imagePosition.y + imageHeight)) {
+      return image_pos3;
     }
     //RENDER IMAGE POSITION 4
-    else if (x >= (windowWidth / 2) + (imageWidth / 2) && x <= windowWidth && y >= (windowHeight / 2) + (imageHeight / 2) && y <= windowHeight) {
-      return <img className="Image" alt="teamMember" src={image_pos4} />
+    else if (cursorPosition.x >= (imagePosition.x + imageWidth) && cursorPosition.x <= screenDefinitions.width && cursorPosition.y >= (imagePosition.y + imageHeight) && cursorPosition.y <= screenDefinitions.height) {
+      return image_pos4;
     }
     //RENDER IMAGE POSITION 5
-    else if (x >= (windowWidth / 2) - (imageWidth / 2) && x <= (windowWidth / 2) + (imageWidth / 2) && y >= (windowHeight / 2) + (imageHeight / 2) && y <= windowHeight) {
-      return <img className="Image" alt="teamMember" src={image_pos5} />
+    else if (cursorPosition.x >= imagePosition.x && cursorPosition.x <= (imagePosition.x + imageWidth) && cursorPosition.y >= (imagePosition.y + imageHeight) && cursorPosition.y <= screenDefinitions.height) {
+      return image_pos5;
     }
     //RENDER IMAGE POSITION 6
-    else if (x > 0 && x < (windowWidth / 2) - (imageWidth / 2) && y >= (windowHeight / 2) + (imageHeight / 2) && y <= windowHeight) {
-      return <img className="Image" alt="teamMember" src={image_pos6} />
+    else if (cursorPosition.x >= 0 && cursorPosition.x <= imagePosition.x && cursorPosition.y >= (imagePosition.y + imageHeight) && cursorPosition.y <= screenDefinitions.height) {
+      return image_pos6;
     }
     //RENDER IMAGE POSITION 7
-    else if (x > 0 && x < (windowWidth / 2) - (imageWidth / 2) && y >= (windowHeight / 2) - (imageHeight / 2) && y <= (windowHeight / 2) + (imageHeight / 2)) {
-      return <img className="Image" alt="teamMember" src={image_pos7} />
+    else if (cursorPosition.x >= 0 && cursorPosition.x <= imagePosition.x && cursorPosition.y >= imagePosition.y && cursorPosition.y <= (imagePosition.y + imageHeight)) {
+      return image_pos7;
     }
     //RENDER DEFAULT IMAGE
     else {
-      return <img className="Image" alt="teamMember" src={default_image} />
+      return default_image;
     }
   }
 
+  renderImage = () => {
+    return (
+      <img className="Image" ref="image" alt="teamMember" src={this.getImageSource()} />
+    );
+  }
+
   render() {
-    return(
+    return (
       <div className="ImageWrapper">
         {this.renderImage()}
       </div>
@@ -89,4 +122,4 @@ class Image extends React.Component {
   }
 }
 
-export default Image;
+export default TestImage;
